792ac5103f825a3adf54ed508c79bf44a223b3bb
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-15 22:26:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    return array.join
+elsif array.count == 2
+  return array.join(" and ")
+elsif array.count > 2
+  last = array.pop
+  return array.join(", ") + ", and " + last
 end
+end
+
+# ["hippo"]
+# ["hippo", "giraffe"] shovel and between hippo and giraffe
+# ["hippo", "giraffe", "monkey", "horse"]
