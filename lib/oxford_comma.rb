e4b4245e1d53e980560e17b7684b6848a69f51b7
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-15 20:27:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.count == 1
     return array.join
 elsif array.count == 2
-  "string".insert(1, and)
+  "string".insert(1)
 end
 # new_array = []
 # puts index[0] + "," + index[1] + ", and" + index[2]
